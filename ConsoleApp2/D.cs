0e105feea47e511c3f0e5683576b714ab276247a
Pravin <nawalepravin007@gmail.com>
2026-05-24 21:35:14 +0530

    Added two projects in one solution and Demostrate the Access Specifiers

---
+++ ConsoleApp2/D.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+namespace ConsoleApp2
+{
+    internal class D : A
+    {
+        static void Main(string[] args)
+        {
+            D d = new D();
+
+            //Console.WriteLine(d.privateVar);
+            //Console.WriteLine(d.internalVar);
+            Console.WriteLine(d.protectedVar);
+            //Console.WriteLine(d.privateProtectedVar);
+            Console.WriteLine(d.protectedInternalVar);
+            Console.WriteLine(d.publicVar);
+        }
+    }
+}
