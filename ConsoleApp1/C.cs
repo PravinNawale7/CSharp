987d974945de180810a51179f19be1b85ed50fe5
Pravin <nawalepravin007@gmail.com>
2026-05-24 21:54:00 +0530

    Initial commit

---
+++ ConsoleApp1/C.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class C : A
+    {
+        public void Show()
+        {
+            //Console.WriteLine(privateVar);
+            Console.WriteLine(internalVar);
+            Console.WriteLine(protectedVar);
+            Console.WriteLine(privateProtectedVar);
+            Console.WriteLine(protectedInternalVar);
+            Console.WriteLine(publicVar);
+            
+        }
+    }
+}
