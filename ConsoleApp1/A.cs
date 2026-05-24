0a65b44302513624f731cfe7adc3a05d58722ac3
Pravin Nawale <159033956+PravinNawale7@users.noreply.github.com>
2026-05-24 21:39:25 +0530

    Added two projects in one solution and Demonstrate the Access Specifiers

---
+++ ConsoleApp1/A.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    public class A
+
+    {
+        private int privateVar = 1;
+        internal int internalVar = 2;
+        protected int protectedVar = 3;
+        private protected int privateProtectedVar = 4;
+        protected internal int protectedInternalVar = 5;
+        public int publicVar = 6;
+
+        public void SameClass()
+        {
+            Console.WriteLine("FROM Class A privateVar " + privateVar);
+            Console.WriteLine("FROM Class A internalVar " + internalVar);
+            Console.WriteLine("FROM Class A protectedVar " + protectedVar);
+            Console.WriteLine("FROM Class A privateProtectedVar " + privateProtectedVar);
+            Console.WriteLine("FROM Class A protectedInternalVar " + protectedInternalVar);
+            Console.WriteLine("FROM Class A publicVar " + publicVar);
+        }
+    }
+}
+
+
---
+++ ConsoleApp1/B.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    public class B
+    {
+        //public void Show()
+        public static void Main()
+        {
+            A a = new A();
+            C c = new C();
+            c.Show();
+
+            a.SameClass();
+            //Console.WriteLine(a.privateVar);
+            Console.WriteLine(a.internalVar);
+            //Console.WriteLine(a.protectedVar);
+            //Console.WriteLine(a.privateProtectedVar);
+            Console.WriteLine(a.protectedInternalVar);
+            Console.WriteLine(a.publicVar);
+
+
+        }
+    }
+}
