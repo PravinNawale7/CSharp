f692f742d0cb0affb0b0466d52312b76ff493c53
Pravin <nawalepravin007@gmail.com>
2026-05-25 22:35:50 +0530

    Added User Registration System with property validation

---
+++ UserRegistrationSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace UserRegistrationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            u.Name = "John Doe";
+            u.Email = "johndoe1@gmail.com";
+            u.Password = "12345678";
+            u.Age = 18;
+
+            u.ShowUser();
+        }
+    }
+}
---
+++ UserRegistrationSystem/User.cs
@@ -0,0 +1,99 @@
+﻿namespace UserRegistrationSystem
+{
+    internal class User
+    {
+        string _name;
+        string _email;
+        string _password;
+        int _age;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if(!string.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Name cannot be empty");
+                }
+            }
+        }
+    
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+
+            set
+            {
+                if (value.Contains("@"))
+                {
+                    _email = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Email");
+                }
+            }
+        }
+
+       
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                if (value.Length >= 8)
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Password must contain 8 characters");
+                }
+            }
+        }
+
+        
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                if (value >= 18 && value <= 60)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Age");
+                }
+            }
+        }
+
+        
+        public void ShowUser()
+        {
+            Console.WriteLine("Name: " + _name);
+            Console.WriteLine("Email: " + _email);
+            Console.WriteLine("Age: " + _age);
+        }
+    }
+}
