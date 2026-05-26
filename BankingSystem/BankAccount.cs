cef3e4881e52f96ea7172a6f86613372776eda6e
Pravin <nawalepravin007@gmail.com>
2026-05-26 11:29:25 +0530

    Implemented Banking System using properties, validation, and object initializers

---
+++ BankingSystem/BankAccount.cs
@@ -0,0 +1,78 @@
+﻿namespace BankingSystem
+{
+    internal class BankAccount
+    {
+        string _accountHolderName;
+        double _balance;
+        int _accountNumber;
+
+        public string AccountHolderName
+        {
+            get
+            {
+                return _accountHolderName;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _accountHolderName = value;
+                }
+            }
+        }
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                if(value >= 0)
+                {
+                    _balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Balance Cannot Be Negative");
+                }
+            }
+        }
+
+        public int AccountNumber
+        {
+            get;                        //Auto Property Initializer
+        } = 1001;
+
+        public void Deposit(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine("Deposit amount must be greater than zero.");
+                return;
+            }
+            else
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposited {amount} . New Balance = {Balance} ");
+            }
+        }
+        public void Withdraw(int amount)
+        {
+            if(amount > Balance)
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+            else
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdraw {amount} SuccessFully. Balance is {Balance}");
+            }
+        }
+        public void ShowBalance()
+        {
+            Console.WriteLine("Account Balance is " + Balance);
+        }
+
+    }
+}
---
+++ BankingSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BankingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount
+            {
+                AccountHolderName = "Rahul Marathe",
+                Balance = 1500
+            };
+            Console.WriteLine(account.AccountNumber);
+            account.Deposit(500);
+            account.Withdraw(2000);
+            account.ShowBalance();
+            
+            
+
+
+
+        }
+    }
+}
