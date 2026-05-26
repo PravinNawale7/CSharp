c9f93adffd421e209e603bead0989f59591895d1
Pravin <nawalepravin007@gmail.com>
2026-05-26 13:28:13 +0530

    Added Student deconstruction using Deconstruct method

---
+++ StudentProfileSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentProfileSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student(
+                "John Doe",
+                20,
+                "Computer Science"
+            );
+
+            var (name, age, course) = s;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Course: {course}");
+        }
+    }
+}
---
+++ StudentProfileSystem/Student.cs
@@ -0,0 +1,45 @@
+﻿namespace StudentProfileSystem
+{
+    internal class Student
+    {
+        string _name;
+        int _age;
+        string _course;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public string Course
+        {
+            get { return _course; }
+            set { _course = value; }
+        }
+
+        public Student(string name, int age, string course)
+        {
+            _name = name;
+            _age = age;
+            _course = course;
+        }
+
+
+
+        public void Deconstruct(out string name,
+                                out int age,
+                                out string course)
+        {
+            name = Name;
+            age = Age;
+            course = Course;
+        }
+    }
+}
