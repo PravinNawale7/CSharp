847b8fec9e38e8e1cfbad1cbf1de4748bb62dd09
Pravin <nawalepravin007@gmail.com>
2026-05-31 11:12:49 +0530

    Add Food Delivery Notification System using delegates

---
+++ FoodDeliveryNotificationSystem/FoodDeliverySystem.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace FoodDeliveryNotificationSystem
+{
+    public delegate void NotificationDelegate();
+
+    internal class FoodDeliverySystem
+    {
+        public void SendEmail()
+        {
+            Console.WriteLine("Email Sent");
+        }
+
+        public void SendSMS()
+        {
+            Console.WriteLine("SMS Sent");
+        }
+
+        public void SendWhatsApp()
+        {
+            Console.WriteLine("WhatsApp Notification Sent");
+        }
+        public void OrderPlaced()
+        {
+            Console.WriteLine("Order Placed");
+
+            NotificationDelegate notify = SendEmail;
+
+            notify += SendSMS;
+            notify += SendWhatsApp;
+
+            
+            notify();
+        }
+    }
+}
---
+++ FoodDeliveryNotificationSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodDeliveryNotificationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FoodDeliverySystem fd = new FoodDeliverySystem();
+
+            fd.OrderPlaced();
+
+            
+        }
+    }
+}
